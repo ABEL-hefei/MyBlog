ce521545605dc7df1b152c99f60b7338a15a07b0
hefei <770368455@qq.com>
2017-11-22 23:27:00 +0800

    整合前端页面，展示首页

--- src/main/java/com/able/Application.java
+++ src/main/java/com/able/Application.java
@@ -1,12 +1,12 @@
-package com.able.MyBlog;
+package com.able;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyBlogApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyBlogApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ src/main/java/com/able/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.able.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//@RestController 返回的是一个对象，在没有页面的情况下，也能看到返回的是一个user对象对应的json字符串，而前端的作用是利用返回的json进行解析渲染页面
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String root() {
+		return "redirect:index";
+	}
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
